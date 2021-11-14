0868098060b5e0df0d9bb03e66ca9258f04c9877
vectorit82 <vektor@ukr.net>
2021-11-14 13:00:58 +0300

    added slider, info, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
 
-});
+    $('.slider__top-inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/arrow-left.svg" alt="предыдущий"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/slider/arrow-right.svg" alt="следующий"></button>',
+    })
+  
+  });
--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,10 @@ function browsersync() {
 }
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src([
+    'node_modules/slick-carousel/slick/slick.css',
+    'app/scss/style.scss'
+  ])
   .pipe(scss({outputStyle: 'compressed'}))
   .pipe(concat('style.min.css'))
   .pipe(autoprefixer({
@@ -34,6 +37,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.min.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
