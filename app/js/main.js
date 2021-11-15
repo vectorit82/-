0a01e6f310c96d23c0c74bcdd290b00c9030ada0
vectorit82 <vektor@ukr.net>
2021-11-15 20:49:01 +0300

    added effects

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,8 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/arrow-left.svg" alt="предыдущий"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/slider/arrow-right.svg" alt="следующий"></button>',
     })
+
+
+    var mixer = mixitup('.catalog__inner');
   
   });
