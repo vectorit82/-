739acb1c67d8360e703a627005eef45501bcc302
vectorit82 <vektor@ukr.net>
2021-11-17 01:37:40 +0300

    added advertising

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,18 @@ $(function () {
     })
 
 
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+    var config = {
+        controls: {
+            scope: 'local'
+        }
+    };
+
+
     var mixer = mixitup('.catalog__inner');
 
     var mixer = mixitup('.action__content');
-  
-  });
+
+});
